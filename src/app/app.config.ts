9a675a8f2f71e2ef9bfc5d3f4798e4834f3943b3
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-08-19 18:54:38 +0530

    build: npm packages installed and integrated and project json config changes added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes)
+    provideRouter(appRoutes),
+    importProvidersFrom(NgbModule),
   ]
 };
