40b29960dd7d748a90ee507b6f3066181ca7859c
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-08-19 19:15:46 +0530

    build: npm packages installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,26 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { pendingRequestsInterceptor$ } from 'ng-http-loader';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
     importProvidersFrom(NgbModule),
+    provideHttpClient(
+      withInterceptors([
+        pendingRequestsInterceptor$,
+      ]),
+    ),
+    provideEnvironmentNgxMask(),
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: 'mediumDate' },
+    },
   ]
 };
