ea542faade7361c7409875e5ba39754d2f4fbe2d
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-08-20 10:54:29 +0530

    feat(feature-dashboard): feature dashboard created

---
+++ src/libs/features/feature-dashboard/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { FeatureDashboard } from './feature-dashboard/feature-dashboard';
+
+export const featureDashboardRoutes: Route[] = [{ path: '', component: FeatureDashboard }];
