5a152c32741291c14d6549a70122e4ade723b7d9
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-08-20 10:58:56 +0530

    fixup(feature-shell): correction added

--- src/libs/features/feature-shell/src/lib/lib.routes.ts
+++ src/libs/features/feature-shell/src/lib/lib.routes.ts
@@ -1,6 +1,3 @@
 import { Route } from '@angular/router';
-import { FeatureShell } from './feature-shell/feature-shell';
 
-export const featureShellRoutes: Route[] = [
-  { path: '', component: FeatureShell },
-];
+export const featureShellRoutes: Route[] = [];
